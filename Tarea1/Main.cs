a0401237ed071b6d2562998b389c82f0fba030a1
virginia soto <viky.zurita@hotmail.com>
2013-03-01 13:24:48 -0600

    Tarea Completa Persona...

---
+++ Tarea1/Main.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Tarea1
+{
+	class MainClass
+	{
+
+		public static void Main(string[] args){
+			
+			Persona persona2 =new Persona(); 
+			Console.WriteLine("Dame tu Nombre");
+			persona2.Nombre=Console.ReadLine();
+			Console.WriteLine("Dame tu Telefono:");
+			persona2.Telefono=Console.ReadLine();
+			Console.WriteLine("Dame tu Email:");
+			persona2.Email=Console.ReadLine();
+			Console.WriteLine("Dame tu Domicilio:");
+			persona2.Domicilio=Console.ReadLine();
+			Console.WriteLine("El nombre es:"+persona2.Nombre);
+			Console.WriteLine("Telefono:"+persona2.Telefono);
+			Console.WriteLine("Email"+persona2.Email);
+			Console.WriteLine("Domicilio:"+persona2.Domicilio);
+		}
+	}
+}
---
+++ Tarea1/Persona.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Tarea1
+{
+	public class Persona
+	{
+		public string Nombre;
+		public string Telefono;
+		public string Email;
+		public string Domicilio;
+
+	}
+}
+
